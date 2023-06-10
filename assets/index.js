66b217affd8d740f43bbe0b91d442c21c150cab7
AnnaSaurina <the_happy_ann@mail.ru>
2023-06-10 17:42:01 +0300

    Added const for mark of car

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+// Создаем переменные для марок авто
+const jaguar = document.getElementById('jaguar');
+const mazda = document.getElementById('mazda');
+const opel = document.getElementById('opel');
+const renoult = document.getElementById('renoult');
+
